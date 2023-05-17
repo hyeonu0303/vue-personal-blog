89a1c4c942cd7736002d07a4f0ac7338af270b79
Parkhyeonwoo <hyeonu030334@gmail.com>
2023-05-17 12:29:51 +0900

    font 추가

--- src/App.vue
+++ src/App.vue
@@ -83,5 +83,17 @@ export default {
 </script>
 
 <style>
-  
+/* @font-face {
+    font-family: 'omyu_pretty';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-01@1.0/omyu_pretty.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+} */
+@font-face {
+    font-family: 'HANAMDAUM';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-2@1.0/HANAMDAUM.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+}
+
 </style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -119,6 +119,17 @@ export default {
 .fade-enter-active{transition:all 1s;}
 .fade-enter-to{opacity: 1;}
 
+@font-face {
+    font-family: 'SUITE-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-2@1.0/SUITE-Regular.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+}
+
+body{
+  font-family: 'SUITE-Regular';
+  font-size:20px;
+}
 
 
 </style>
